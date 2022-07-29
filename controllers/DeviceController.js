1ed80c12f9007ec74a893ac08946bc17f0929b04
Misha-Kirichenko <m.kirichenko.dev@gmail.com>
2022-07-29 23:33:04 +0400

    uploading logic modified

--- controllers/DeviceController.js
+++ controllers/DeviceController.js
@@ -1,5 +1,6 @@
 const uuid = require("uuid");
 const path = require("path");
+const fs = require("fs");
 
 const { Device } = require("../models");
 
@@ -7,6 +8,7 @@ exports.add = async (req, res) => {
   const { name, price, brandId, typeId, descr } = req.body;
   const { img } = req.files;
   const whiteList = ["image/jpeg", "image/png"];
+  const uploadPath = "./img/device-imgs";
   const errors = [];
 
   try {
@@ -39,7 +41,11 @@ exports.add = async (req, res) => {
       const imgExt = imgInfo[imgInfo.length - 1];
       const fileName = `${uuid.v4()}.${imgExt}`;
 
-      await img.mv(path.resolve(__dirname, "..", "img/device-imgs", fileName));
+      if (!fs.existsSync(uploadPath)) {
+        fs.mkdirSync(uploadPath, { recursive: true });
+      }
+
+      await img.mv(path.resolve(__dirname, "..", uploadPath, fileName));
 
       const deviceCreated = await Device.create({
         name,
